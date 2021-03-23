077b77c167a90521f515ac8da5109330875751fd
thatguyish <ishmell.ramirez@gmail.com>
2021-03-23 19:08:34 -0400

    finished with session included

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
-from flask import Flask, render_template, request,redirect,flash
+import sys
+from flask import Flask, render_template, request,redirect,flash, session
 from surveys import satisfaction_survey
 app = Flask('__name__')
 app.config["SECRET_KEY"]= "secret-key"
-responses = []
+
 
 @app.route('/')
 def home():
-    return render_template('base.html',survey = satisfaction_survey)
+    if not session.get('responses'):
+        return render_template('base.html',survey = satisfaction_survey)
+    if len(satisfaction_survey.questions)==len(session['responses']):
+        return redirect('/thank_you')
+    
+
+@app.route('/start-session',methods=['POST'])
+def start_session():
+        session["responses"]=[]
+        return redirect('/questions/0')
 
 @app.route('/questions/<int:current_question>')
 def show_question(current_question):
-    if current_question!=len(responses):
+    if current_question!=len(session["responses"]):
         flash("almost went to the wrong place")
-        return redirect(f'/questions/{len(responses)}')
-    elif len(responses)==len(satisfaction_survey.questions):
+        return redirect(f'/questions/{len(session["responses"])}')
+    elif len(session)==len(satisfaction_survey.questions):
         redirect('/thank_you')
     question = satisfaction_survey.questions[current_question]
     return render_template('questions.html',question=question,survey =satisfaction_survey,q_id=current_question)
 
-@app.route('/answer')
+@app.route('/answer',methods=['POST','GET'])
 def answer_view():
-    answer = request.args["answer"]
+    answer = request.form['answer']
+    responses = session["responses"]
     responses.append(answer)
-    if len(responses)>=len(satisfaction_survey.questions):
+    session['responses']=responses
+    if len(session['responses'])>=len(satisfaction_survey.questions):
         return redirect('/thank_you')
-    return redirect(f'/questions/{len(responses)}')
+    return redirect(f'/questions/{len(session["responses"])}')
 
 @app.route('/thank_you')
 def thank_you_view():
-    if len(responses)!=len(satisfaction_survey):
+    if len(session["responses"])!=len(satisfaction_survey.questions):
         redirect('/')
-    return render_template('thank-you.html',survey=satisfaction_survey,responses=responses)
+    return render_template('thank-you.html',survey=satisfaction_survey)
